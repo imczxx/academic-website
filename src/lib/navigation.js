d87402f29fefdac2c1494a175bce0ec20de04872
Zhenxing Chen <chenzhenxing2021@gmail.com>
2025-01-21 09:55:52 +0800

    temp

--- src/lib/navigation.js
+++ src/lib/navigation.js
@@ -3,25 +3,25 @@ import { Home, BookOpen, Mail, User, Star } from 'lucide-svelte';
 export const navigation = [
 	{
 		path: '#about',
-		name: '关于',
+		name: 'about',
 		icon: User,
 		order: 1
 	},
 	{
 		path: '#research',
-		name: '研究',
+		name: 'research',
 		icon: Star,
 		order: 2
 	},
 	{
 		path: '#publications',
-		name: '发表',
+		name: 'publications',
 		icon: BookOpen,
 		order: 3
 	},
 	{
 		path: '#contact',
-		name: '联系',
+		name: 'contact',
 		icon: Mail,
 		order: 4
 	}
