9093895e274217b69fdacc4e03f0b6b9b9f8cd05
Zhenxing Chen <chenzhenxing2021@gmail.com>
2025-01-26 11:18:51 +0800

    tepm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,6 +33,23 @@
 
 <div class="layout">
 	{#if isMobile}
+		<button
+			class="fixed top-4 left-4 z-50 p-2 rounded-lg bg-gray-100 dark:bg-gray-800 shadow-lg"
+			on:click={responsiveStore.toggleDrawer}
+			aria-label="Toggle navigation menu">
+			<svg 
+				xmlns="http://www.w3.org/2000/svg" 
+				class="h-6 w-6 text-gray-700 dark:text-gray-300" 
+				fill="none" 
+				viewBox="0 0 24 24" 
+				stroke="currentColor">
+				{#if isDrawerOpen}
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+				{:else}
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+				{/if}
+			</svg>
+		</button>
 		<!-- Mobile drawer -->
 		<div 
 			class="fixed inset-0 z-40 transform transition-transform duration-300 ease-in-out {isDrawerOpen ? 'translate-x-0' : '-translate-x-full'}"
@@ -48,6 +65,13 @@
 				toggleTheme={themeStore.toggleTheme}
 			/>
 		</div>
+		{#if isMobile && isDrawerOpen}
+			<div 
+				class="fixed inset-0 bg-black/50 z-30"
+				on:click={responsiveStore.closeDrawer}
+				aria-hidden="true"
+			/>
+		{/if}
 	{:else}
 		<!-- Desktop sidebar -->
 		<Navigation 
